2c645be202801d62a857ed2cb171d10c09042f6b
Jianwei Zhao <45277090+JWZhao-uestc@users.noreply.github.com>
2025-07-29 19:59:44 +0800

    Update inference_per_class.py

--- inference_per_class.py
+++ inference_per_class.py
@@ -19,52 +19,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 
-
-import numpy as np
-from medpy.metric.binary import __surface_distances
-
-# region
-# def normalized_surface_dice(a: np.ndarray, b: np.ndarray, threshold: float, spacing: tuple = None, connectivity=1):
-#     """
-#     This implementation differs from the official surface dice implementation! These two are not comparable!!!!!
-
-#     The normalized surface dice is symmetric, so it should not matter whether a or b is the reference image
-
-#     This implementation natively supports 2D and 3D images. Whether other dimensions are supported depends on the
-#     __surface_distances implementation in medpy
-
-#     :param a: image 1, must have the same shape as b
-#     :param b: image 2, must have the same shape as a
-#     :param threshold: distances below this threshold will be counted as true positives. Threshold is in mm, not voxels!
-#     (if spacing = (1, 1(, 1)) then one voxel=1mm so the threshold is effectively in voxels)
-#     must be a tuple of len dimension(a)
-#     :param spacing: how many mm is one voxel in reality? Can be left at None, we then assume an isotropic spacing of 1mm
-#     :param connectivity: see scipy.ndimage.generate_binary_structure for more information. I suggest you leave that
-#     one alone
-#     :return:
-#     """
-#     assert all([i == j for i, j in zip(a.shape, b.shape)]), "a and b must have the same shape. a.shape= %s, " \
-#                                                             "b.shape= %s" % (str(a.shape), str(b.shape))
-#     if spacing is None:
-#         spacing = tuple([1 for _ in range(len(a.shape))])
-#     a_to_b = __surface_distances(a, b, spacing, connectivity)
-#     b_to_a = __surface_distances(b, a, spacing, connectivity)
-
-#     numel_a = len(a_to_b)
-#     numel_b = len(b_to_a)
-
-#     tp_a = np.sum(a_to_b <= threshold) / numel_a
-#     tp_b = np.sum(b_to_a <= threshold) / numel_b
-
-#     fp = np.sum(a_to_b > threshold) / numel_a
-#     fn = np.sum(b_to_a > threshold) / numel_b
-
-#     dc = (tp_a + tp_b) / (tp_a + tp_b + fp + fn + 1e-8)  # 1e-8 just so that we don't get div by 0
-#     return dc
-# endregion
-
-from SurfaceDice import compute_surface_distances, compute_surface_dice_at_tolerance, compute_dice_coefficient
-
 def jaccard(y_true, y_pred):
     intersection = (y_true * y_pred).sum()
     union = y_true.sum() + y_pred.sum() - intersection
@@ -288,12 +242,9 @@ def main(args):
                 ##### all labels query value
                 dice = compute_dice(gt>0, seg_map>0)
                 sam_dice_scores.append(dice)
-                #iou = jaccard(gt>0, seg_map>0)
-                from medpy.metric.binary import asd, hd95
+                iou = jaccard(gt>0, seg_map>0)
                 if np.sum(seg_map>0) == 0 or np.sum(gt>0) == 0:
                     continue
-                # surface_distances = compute_surface_distances(gt>0, seg_map>0)
-                # iou = compute_surface_dice_at_tolerance(surface_distances, 1)
                 iou = hd95(seg_map>0, gt>0, 0.5, 2)
                 sam_iou_scores.append(iou)
                 ##### per label 
